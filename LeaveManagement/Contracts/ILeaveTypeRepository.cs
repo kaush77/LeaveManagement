9d24fa1d55011215435a3d3c865d0f77225cb8d2
kaush77 <kaushlender.k@gmail.com>
2020-02-03 10:39:00 -0800

    Add project files.

---
+++ LeaveManagement/Contracts/ILeaveTypeRepository.cs
@@ -0,0 +1,13 @@
+﻿using LeaveManagement.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LeaveManagement.Contracts
+{
+    interface ILeaveTypeRepository : IRepositoryBase<LeaveType>
+    {
+        ICollection<LeaveType> GetEmployeeByLeaveType(int id);
+    }
+}
